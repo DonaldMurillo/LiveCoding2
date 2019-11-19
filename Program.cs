95cbd6af49a7b361fdd8e3d67a988b05f4200e14
Donald Murillo <megalion07@hotmail.com>
2019-11-19 18:35:25 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LiveCoding2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
